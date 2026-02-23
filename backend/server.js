31862b18d173a7ec5c98c2b810c372d703756816
Ujjwal Paliwal <164433626+Ujjwalpaliwal@users.noreply.github.com>
2026-02-23 18:19:49 +0530

    Refactor production route handling in server.js

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,10 @@ app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
 }
 
 app.listen(PORT, () => {
